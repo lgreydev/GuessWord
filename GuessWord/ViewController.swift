81113bd9a12bba64b33dae0755d8517163d5b955
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-05-18 17:55:02 +0300

    Connect storyboard

--- GuessWord/ViewController.swift
+++ GuessWord/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Property
+    @IBOutlet weak var lifeLabel: UILabel!
+    @IBOutlet weak var levelLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var wordLabel: UILabel!
+ 
+    
+    // MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    // MARK: - Action
+    @IBAction func pressedButtons(_ sender: UIButton) {
+        let letter = sender.titleLabel?.text ?? "no letter"
+        print(letter)
+    }
 }
 
